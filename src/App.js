d07af6f371ed6467369f941b8c25379839ce905b
Vaseem <skvaseem999@gmail.com>
2020-03-28 22:57:42 +0530

    New file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import Person from './Person/Person' ;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return(
+      <div className="App">
+        <h1>Hello new lappy ,start with REACT</h1>
+        <p>Only one root element is allowed in JSX</p>
+        <Person />
+      </div>
+    );
+  }
 }
 
 export default App;
